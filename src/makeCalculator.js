62c2f3dfcb81a9bd4d055aa18039c2e8a7fe8636
Marcos Vinicius <marcos_araujo7@hotmail.com>
2026-02-12 11:13:07 -0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,39 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  const object = {
+    result: 0,
+
+    operate(callback, number) {
+      callback(number);
+
+      return object;
+    },
+
+    reset() {
+      object.result = 0;
+
+      return object;
+    },
+
+    add(number) {
+      object.result += number;
+    },
+
+    subtract(number) {
+      object.result -= number;
+    },
+
+    multiply(number) {
+      object.result *= number;
+    },
+
+    divide(number) {
+      object.result /= number;
+    },
+  };
+
+  return object;
 }
 
 module.exports = makeCalculator;
